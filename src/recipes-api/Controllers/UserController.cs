7e505b5ddc93d41c28e486207ee16abde8337caa
Enio Schaefer <enio100schaefer@hotmail.com>
2023-07-19 22:15:56 -0300

    feat: Req. 6

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -1,50 +1,53 @@
 using Microsoft.AspNetCore.Mvc;
 using recipes_api.Services;
-using recipes_api.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace recipes_api.Controllers;
 
 [ApiController]
 [Route("user")]
 public class UserController : ControllerBase
-{    
-    public readonly IUserService _service;
-    
-    public UserController(IUserService service)
-    {
-        this._service = service;        
-    }
+{
+  public readonly IUserService _service;
 
-    // 6 - Sua aplicação deve ter o endpoint GET /user/:email
-    [HttpGet("{email}", Name = "GetUser")]
-    public IActionResult Get(string email)
-    {                
-        throw new NotImplementedException();
-    }
+  public UserController(IUserService service)
+  {
+    this._service = service;
+  }
 
-    // 7 - Sua aplicação deve ter o endpoint POST /user
-    [HttpPost]
-    public IActionResult Create([FromBody]User user)
+  // 6 - Sua aplicação deve ter o endpoint GET /user/:email
+  [HttpGet("{email}", Name = "GetUser")]
+  public IActionResult Get(string email)
+  {
+    try
     {
-        throw new NotImplementedException();
-    }
+      User user = _service.GetUser(email);
 
-    // "8 - Sua aplicação deve ter o endpoint PUT /user
-    [HttpPut("{email}")]
-    public IActionResult Update(string email, [FromBody]User user)
-    {
-        throw new NotImplementedException();
+      return Ok(user);
     }
-
-    // 9 - Sua aplicação deve ter o endpoint DEL /user
-    [HttpDelete("{email}")]
-    public IActionResult Delete(string email)
+    catch (Exception error)
     {
-        throw new NotImplementedException();
-    } 
+      return NotFound(error.Message);
+    }
+  }
+
+  // 7 - Sua aplicação deve ter o endpoint POST /user
+  [HttpPost]
+  public IActionResult Create([FromBody] User user)
+  {
+    throw new NotImplementedException();
+  }
+
+  // "8 - Sua aplicação deve ter o endpoint PUT /user
+  [HttpPut("{email}")]
+  public IActionResult Update(string email, [FromBody] User user)
+  {
+    throw new NotImplementedException();
+  }
+
+  // 9 - Sua aplicação deve ter o endpoint DEL /user
+  [HttpDelete("{email}")]
+  public IActionResult Delete(string email)
+  {
+    throw new NotImplementedException();
+  }
 }
